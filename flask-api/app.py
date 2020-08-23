895deb8a0f238323a9a4040074cb91aa7a89a9f8
seyoung <airseyoung@naver.com>
2020-08-24 00:26:11 +0900

    Add : team enroll
    새로운 팀 추가하는 엔드포인트 구현

--- flask-api/app.py
+++ flask-api/app.py
@@ -7,11 +7,10 @@ from flask_jwt_extended import JWTManager, create_access_token
 
 app = Flask(__name__)
 
-app.config['MYSQL_USER'] = ''
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = ''
-app.config['MYSQL_CURSORCLASS'] = ''
 app.config['JWT_SECRET_KEY'] = 'secret'
+app.config['MYSQL_DATABASE_CHARSET'] = 'utf-8'
+
+status = ['VALID', 'INVALID']
 
 jwt = JWTManager(app)
 CORS(app)
@@ -22,7 +21,7 @@ cur = mysql.connect(user='seyoung', password='seyoung', host='localhost', databa
 
 @app.route('/users/register', methods=['POST'])
 def register():
-    cursor = cur.cursor()
+    cursor = cur.cursor(buffered=True)
     id = request.get_json()['id']
     password = bcrypt.generate_password_hash(
         request.get_json()['password']).decode('utf-8')
@@ -32,10 +31,10 @@ def register():
 
     reg_date = datetime.utcnow()
 
-    sql = """insert into users(id,password, name, tel, email, reg_date)
-             values (%s, %s, %s, %s, %s, %s)"""
+    sql = """insert into users(id,password, name, tel, email, reg_date, status)
+             values (%s, %s, %s, %s, %s, %s,%s)"""
 
-    cursor.execute(sql,(str(id), str(password), str(name), str(tel), str(email), str(reg_date)))
+    cursor.execute(sql,(str(id), str(password), str(name), str(tel), str(email), str(reg_date), str(status[0])))
     cur.commit()
 
     result = {
@@ -51,7 +50,7 @@ def register():
 
 @app.route('/users/login', methods=['POST'])
 def login():
-    cursor = cur.cursor()
+    cursor = cur.cursor(buffered=True)
     id = request.get_json()['id']
     password = request.get_json()['password']
     access_date = datetime.utcnow()
@@ -78,6 +77,39 @@ def login():
 
     return result
 
+@app.route('/teams/enroll', methods=['POST'])
+def enroll():
+    cursor = cur.cursor(buffered=True)
+    name = request.get_json()['teamname']
+    id = request.get_json()['id']
+
+    cursor.execute("SELECT name FROM users where id = '" + str(id) + "'")
+    row = cursor.fetchone()
+    rowcount = cursor.rowcount
+
+    created_by = row[0]
+
+    cursor.execute("SELECT * FROM team where name ='" + str(name) + "'")
+    count = cursor.rowcount
+
+    if count == 0 and rowcount != 0:
+        sql = """insert into team(name, status, created_by)
+                values (%s, %s, %s)"""
+        cursor.execute(sql, (str(name), str(status[0]), str(created_by)))
+        cur.commit()
+
+        result = {
+            'name': name,
+            'created_by': created_by
+        }
+        rtn = {'result': result}
+        result = json.dumps(rtn, default=str)
+
+    else:
+        message = {'duplicate': "Dupicate teamname"}
+        result = json.dumps(message, default=str)
+
+    return result
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000)
